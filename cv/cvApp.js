a2d480e631bb18cf977e5b394db74c35dd574f32
seharpanesar <sehar.panesar@gmail.com>
2022-07-03 18:32:10 -0700

    worked on navbar transition into cv

--- cv/cvApp.js
+++ cv/cvApp.js
@@ -1,20 +1,29 @@
-// todo: make screen white
-
-const Texts =[
-    "HTML", "CSS", "JAVASCRIPT", "SWIFT", "MONGOOSE", "REACT", "PYTHON", "SASS",
-    "DJANGO", "NODEJS", "MONGODB", "EXPRESS", "MYSQL", "JQUERY", "JAVA"
+const Texts = [
+    'HTML', 'CSS', 'JAVASCRIPT',
+    'SWIFT', 'MONGOOSE', 'REACT',
+    'PYTHON', 'SASS',
+    'DJANGO', 'NODEJS', 'MONGODB',
+    'EXPRESS', 'MYSQL', 'JQUERY', 'JAVA'
 ];
 
-var tagCloud = TagCloud(".Sphere", Texts, {
+var tagCloud = TagCloud('.Sphere', Texts, {
+
+    // Sphere radius in px
     radius: 230,
 
-    maxSpeed: "normal",
-    initSpeed: "fast",
+    // animation speed
+    // slow, normal, fast
+    maxSpeed: 'normal',
+    initSpeed: 'fast',
 
+    // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
     direction: 135,
 
+    // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
     keep: true
+
 });
 
-var color = "#FF5733";
-document.querySelector(".Sphere").style.color = color;
+// Giving color to each text in sphere
+var color = '#FF5733 ';
+document.querySelector('.Sphere').style.color = color;
--- home/app.js
+++ home/app.js
@@ -1,4 +1,4 @@
-const hero = document.querySelector(".hero");
+const home = document.querySelector(".home");
 const slider = document.querySelector(".slider");
 const headline = document.querySelector(".name");
 const info = document.querySelector(".intro_text")
@@ -10,7 +10,7 @@ const navbarlinks = document.querySelector(".nav-links");
 // chain animations with this obj 
 const tl = new TimelineMax();
 
-tl.fromTo(hero,1,{height:"0%"}, {height:"100%", ease: Power2.easeInOut})
+tl.fromTo(home,1,{height:"0%"}, {height:"100%", ease: Power2.easeInOut})
 .fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0})
 .fromTo(info,1,{opacity:0}, {opacity:1}, "-=0.5")
 .fromTo(navbarlinks, 1, {opacity: 0}, {opacity: 1}, "-=0.5")
