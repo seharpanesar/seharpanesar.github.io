972a0179e0df28186b7fdeee26427e3848a613d2
seharpanesar <sehar.panesar@gmail.com>
2022-07-11 19:15:53 -0700

    added education to cv

--- cv/cvApp.js
+++ cv/cvApp.js
@@ -1,19 +1,39 @@
+courses = document.querySelectorAll('.bach,.mast');
+
+for (var i = 0 ; i < courses.length; i++) {
+    courses[i].addEventListener('mouseenter', changeBackground, false ) ; 
+    courses[i].addEventListener('mouseleave', backToNormal, false ) ;
+ }
+    
+function changeBackground(event) {
+    object = event.target
+    var paragraph = document.querySelector(".description");
+    var text = document.createTextNode(object.getElementsByClassName("course-desc")[0].innerHTML);
+
+    paragraph.appendChild(text);
+}
+
+function backToNormal(event) {
+    var paragraph = document.querySelector(".description");
+    while(paragraph.firstChild) {
+        paragraph.removeChild(paragraph.firstChild);
+    }
+}
+
 const Texts = [
-    'HTML', 'CSS', 'JAVASCRIPT',
-    'SWIFT', 'MONGOOSE', 'REACT',
-    'PYTHON', 'SASS',
-    'DJANGO', 'NODEJS', 'MONGODB',
-    'EXPRESS', 'MYSQL', 'JQUERY', 'JAVA'
+    "Python", "Java", "C / C++", 
+    "Git", "Pandas / Numpy", "TensorFlow / PyTorch", 
+    "HTML", "JavaScript", "CSS", "Linux / Unix", 
+    "SQL", "AWS"
 ];
 
-var tagCloud = TagCloud('.Sphere', Texts, {
-
+var tagCloud = TagCloud('.sphere', Texts, {
     // Sphere radius in px
     radius: 230,
 
     // animation speed
     // slow, normal, fast
-    maxSpeed: 'normal',
+    maxSpeed: 'medium',
     initSpeed: 'fast',
 
     // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
@@ -21,9 +41,8 @@ var tagCloud = TagCloud('.Sphere', Texts, {
 
     // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
     keep: true
-
 });
 
 // Giving color to each text in sphere
-var color = '#FF5733 ';
-document.querySelector('.Sphere').style.color = color;
+var color = '#F55723';
+document.querySelector('.sphere').style.color = color;
