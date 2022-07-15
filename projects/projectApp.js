00e2cdb736691eb17f41e88da94d9c38c0cd904b
seharpanesar <sehar.panesar@gmail.com>
2022-07-14 21:22:57 -0700

    added github links

---
+++ projects/projectApp.js
@@ -0,0 +1,60 @@
+document.getElementById("chess").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/ChessJava',
+        '_blank'
+    );
+};
+document.getElementById("poker").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/Poker',
+        '_blank'
+    );
+};
+document.getElementById("nlp").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/CS-577-Project',
+        '_blank'
+    );
+};
+document.getElementById("scrabble").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/ScrabbleCheater',
+        '_blank'
+    );
+};
+document.getElementById("kenken").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/Kenken',
+        '_blank'
+    );
+};
+document.getElementById("blackjack").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/BlackjackWinnings',
+        '_blank'
+    );
+};
+document.getElementById("chaos").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/ChaosGame',
+        '_blank'
+    );
+};
+document.getElementById("nim").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/TheGameOfNIM',
+        '_blank'
+    );
+};
+document.getElementById("coin").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/HeadsTailsApp',
+        '_blank'
+    );
+};
+document.getElementById("site").onclick = function () {
+    window.open(
+        'https://github.com/seharpanesar/Site',
+        '_blank'
+    );
+};
