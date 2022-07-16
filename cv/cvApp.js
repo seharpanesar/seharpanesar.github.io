1a0693256c1395f5586974ef372e8fb4894af6d0
seharpanesar <sehar.panesar@gmail.com>
2022-07-15 18:39:17 -0700

    added gsap fade animations

--- cv/cvApp.js
+++ cv/cvApp.js
@@ -1,3 +1,58 @@
+gsap.registerPlugin(ScrollTrigger);
+
+exp_header = document.querySelectorAll(".exp_header")
+exp_cards = document.querySelectorAll(".exp_cards") // 3 cards
+edu_header = document.querySelectorAll(".edu_header")
+edu_desc = document.querySelectorAll(".edu_desc") // text 
+edu_box =  document.querySelectorAll(".description-container")
+edu_grid = document.querySelectorAll(".bach, .mast, .sem") // grid 
+edu_legend = document.querySelectorAll(".legend-entry") // 2 of them
+skills_header = document.querySelectorAll(".skills-header") 
+skills_cloud = document.querySelectorAll(".sphere")
+skills_bar = document.querySelectorAll(".bar")
+skills_bar_labels =document.querySelectorAll(".bar-labels") //
+
+var order = [exp_header, exp_cards, edu_header, edu_desc, edu_box, edu_grid, 
+    edu_legend, skills_header, skills_cloud, skills_bar, skills_bar_labels]
+
+
+    //https://www.youtube.com/watch?v=ygcEKd0RIGg
+
+
+let t1 = new TimelineMax();
+
+t1.fromTo(exp_header,0.5,{opacity:0, y:20}, {opacity:1, y:0, duration: 0.3})
+.fromTo(exp_cards, {opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+.fromTo(edu_header,{opacity:0, y:20}, {opacity:1, y:0,duration: 0.3})
+.fromTo(edu_desc,  {opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+// .fromTo(edu_box, {opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+// .fromTo(edu_grid, {opacity: 0, y:20}, { opacity: 1, y:0,duration: 0.3})
+// .fromTo(edu_legend, {opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+// .fromTo(skills_header, {opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+// .fromTo(skills_cloud,  {opacity: 0, y:20}, { opacity: 1, y:0,duration: 0.3})
+// .fromTo(skills_bar, {opacity: 0, y:20}, { opacity: 1, y:0,duration: 0.3})
+// .fromTo(skills_bar_labels, { opacity: 0, y:20}, {opacity: 1, y:0,duration: 0.3})
+
+
+let t2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".dark",
+        start: "center-bottom"
+    }
+});
+
+t2.from(edu_box, {opacity: 0, y:20})
+.from(edu_grid, {opacity: 0, y:20})
+.from(edu_legend, {opacity: 0, y:20})
+.from(skills_header, {opacity: 0, y:20})
+.from(skills_cloud,  {opacity: 0, y:20})
+.from(skills_bar, {opacity: 0, y:20})
+.from(skills_bar_labels, { opacity: 0, y:20})
+
+
+
+
+// animation for courses description display 
 courses = document.querySelectorAll('.bach,.mast');
 
 for (var i = 0 ; i < courses.length; i++) {
@@ -20,6 +75,7 @@ function backToNormal(event) {
     }
 }
 
+// tagcloud animation
 const Texts = [
     "Python", "Java", "C / C++", "AWS", 
     "Git", "Pandas / Numpy", "TensorFlow / PyTorch", 
@@ -28,36 +84,23 @@ const Texts = [
 ];
 
 var tagCloud = TagCloud('.sphere', Texts, {
-    // Sphere radius in px
     radius: 230,
 
-    // animation speed
-    // slow, normal, fast
     maxSpeed: 'fast',
     initSpeed: 'medium',
 
-    // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
     direction: 135,
 
-    // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
     keep: true
 });
 
-// Giving color to each text in sphere
-var color = '#F795AF';
-document.querySelector('.sphere').style.color = color;
 
 // color each skill in tagcloud
-
 all_items = document.querySelectorAll('.tagcloud--item')
-
-var first = '#f08f90'
-var second = '#c72eff'
-var third = '#2d95f7'
 
 const color_dict = {
     "Python": "#eb4034", 
-    "Java": second,
+    "Java": '#c72eff',
     "C / C++": "#861be3", 
     "Git":"#f57acc",
     "Pandas / Numpy":"#fa0f94",
@@ -65,13 +108,12 @@ const color_dict = {
     "HTML":"#861be3", 
     "JavaScript":"#861be3",
     "CSS":"#861be3",
-    "Linux / Unix":second, 
-    "SQL": third, 
-    "AWS": third
+    "Linux / Unix":'#c72eff', 
+    "SQL": '#2d95f7', 
+    "AWS": '#2d95f7'
 }
 
 for (var i = 0 ; i < all_items.length; i++) {
-
     var curr_color = color_dict[all_items[i].innerHTML]
     all_items[i].style.color = curr_color; 
  } 
